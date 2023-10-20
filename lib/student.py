df0e541c83c6b292e6adef3b0d86d28f2f96ac7f
Levy <edgarlevy81@gmail.com>
2023-10-20 03:27:19 +0300

    Lab done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,15 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [] #Initialize empty list
     
-    def learn(self):
+    def learn(self,expand_knowledge):
         pass
+        self.knowledge.append(expand_knowledge)
+        
+
+student = Student("Robert","Green")
+student.learn("Python is a versatile programming language")
+print(student.knowledge)
